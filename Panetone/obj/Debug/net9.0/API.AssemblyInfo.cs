cbd1acb423f7ffad3119a2eca15f2f2663d9fe27
thaiis7 <thaismichel7@gmail.com>
2026-05-28 13:46:00 -0300

    add aula03

--- Panetone/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Panetone/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c71c6b71fa95086d14eee854c011a39d61cc99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b828463dad0ffc495ece85a7a75a0681359e4953")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- api/obj/Debug/net9.0/docker-notes.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/docker-notes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("docker-notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828bdb904b781e399f012917d2b2444598b45681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b828463dad0ffc495ece85a7a75a0681359e4953")]
 [assembly: System.Reflection.AssemblyProductAttribute("docker-notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("docker-notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
